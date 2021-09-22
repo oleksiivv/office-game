e47115e3fdb2dfc394abd5f8b958572ed8095743
Alex <oleksii.petrochenko.pz.2019@lpnu.ua>
2021-09-22 12:03:45 +0300

    [CS-1] add all C# files

---
+++ Office Game/Assets/LevelsPanelController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LevelsPanelController : MonoBehaviour
+{
+    public Image[] levels;
+
+    public Color32 activeItem;
+    public Color32 unavailableItem;
+
+    public ScenesManager scenes;
+
+    void Start(){
+        //PlayerPrefs.SetInt("currentLevel", 1);
+        for(int i=0;i<levels.Length;i++){
+            if(i<=PlayerPrefs.GetInt("currentLevel")){
+                levels[i].GetComponent<Image>().color = activeItem;
+            }
+            else{
+                levels[i].GetComponent<Image>().color = unavailableItem;
+            }
+        }
+    }
+
+    public void openLevel(int level){
+        if(PlayerPrefs.GetInt("currentLevel",0) >= level-1){
+            scenes.openScene(level+1);
+        }
+    }
+}
---
+++ Office Game/Assets/PlayerUI.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class PlayerUI : MonoBehaviour
+{
+    public GameObject winPanel, deathPanel, pausePanel;
+
+    public ScenesManager scenes;
+
+    private string gameID = "4356945";
+    public static int addCnt = 1;
+
+    void Start(){
+        Advertisement.Initialize(gameID, false);
+    }
+
+    public void pause(){
+        pausePanel.SetActive(true);
+        Time.timeScale = 0;      
+
+        if(addCnt % 3 == 0){
+            if(Advertisement.IsReady("Interstitial_Android")){
+                Advertisement.Show("Interstitial_Android");
+            }
+        }
+
+        addCnt++;
+    }
+
+    public void resume(){
+        pausePanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void openScene(int id){
+        Time.timeScale = 1;
+        scenes.openScene(id);
+        //Application.LoadLevel(id);
+    }
+
+    public void restart(){
+        openScene(Application.loadedLevel);
+    }
+
+    public void next(){
+        openScene(Application.loadedLevel+1);
+    }
+
+    public void setDeathPanelVisible(bool visible){
+        deathPanel.SetActive(visible);
+
+        if(visible){
+            if(addCnt % 3 == 0){
+                if(Advertisement.IsReady("Interstitial_Android")){
+                    Advertisement.Show("Interstitial_Android");
+                }
+            }
+
+            addCnt++;   
+        }
+    }
+
+    public void setWinPanelVisible(bool visible){
+        winPanel.SetActive(visible);
+    }
+}
